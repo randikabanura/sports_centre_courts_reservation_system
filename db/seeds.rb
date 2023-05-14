a778bfb24a9798ab51b3147b3bafe643eda804cd
Banura Randika <randika.banura@gmail.com>
2023-05-14 23:10:16 +0530

    seed file update add

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,10 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 
-4.times do |index|
-  Court.create(name: "#{(index + 1).humanize} court", court_type: index, active: true)
+if Court.count.zero?
+  4.times do |index|
+    Court.create(name: "#{(index + 1).humanize} court", court_type: index, active: true)
+  end
 end
 
-Customer.create!([{provider: "email", uid: "test@test.com", password: "password"}])
+Customer.create!([{ provider: "email", uid: "test@test.com", email: "test@test.com", password: "password" }])
